1b653984c51d1488948974245bb85f8ca7bc1bf0
guillaume alisse <gualisse@gmail.com>
2020-11-19 22:17:59 +0100

    modification des merge incorrect

--- deploy.bat
+++ deploy.bat
@@ -5,15 +5,11 @@
 
 
 for %%x in (
-<<<<<<< HEAD
-biblio-api\target\biblio-api-1.0.1-SNAPSHOT.jar
-biblio-front\target\biblio-front-1.0.1-SNAPSHOT.jar
-biblio-batch\target\biblio-batch-1.0.1-SNAPSHOT.jar
-=======
+
 biblio-api\target\biblio-api-1.1.1-SNAPSHOT.jar
 biblio-front\target\biblio-front-1.1.1-SNAPSHOT.jar
 biblio-batch\target\biblio-batch-1.1.1-SNAPSHOT.jar
->>>>>>> feature/ListeDattenteReservationLivre
+
   ) do (
 start java -jar %%x
 )
