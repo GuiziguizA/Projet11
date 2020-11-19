3678ab7ffa283a7443e5e146982d32d12215c2f5
guillaume alisse <gualisse@gmail.com>
2020-11-19 21:06:00 +0100

    modif des version des jar

--- deploy.bat
+++ deploy.bat
@@ -5,9 +5,9 @@
 
 
 for %%x in (
-biblio-api\target\biblio-api-0.0.1-SNAPSHOT.jar
-biblio-front\target\biblio-front-0.0.1-SNAPSHOT.jar
-biblio-batch\target\biblio-batch-0.0.1-SNAPSHOT.jar
+biblio-api\target\biblio-api-1.1.1-SNAPSHOT.jar
+biblio-front\target\biblio-front-1.1.1-SNAPSHOT.jar
+biblio-batch\target\biblio-batch-1.1.1-SNAPSHOT.jar
   ) do (
 start java -jar %%x
 )
