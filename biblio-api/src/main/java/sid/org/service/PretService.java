e675ce6fc556d969bd46c8aa456ed3295128aa68
guillaume alisse <gualisse@gmail.com>
2020-11-19 22:55:14 +0100

    modification des merge incorrect

--- biblio-api/src/main/java/sid/org/service/PretService.java
+++ biblio-api/src/main/java/sid/org/service/PretService.java
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-package sid.org.service;
-
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-
-import sid.org.classe.Pret;
-import sid.org.exception.EntityAlreadyExistException;
-import sid.org.exception.LivreIndisponibleException;
-import sid.org.exception.ResultNotFoundException;
-
-public interface PretService {
-
-	public Pret creerPret(Long idLivre, String mail)
-			throws ResultNotFoundException, LivreIndisponibleException, EntityAlreadyExistException;
-
-	public void supprimerPret(Long id) throws ResultNotFoundException;
-
-	public Page<Pret> afficherPrets(String mail, int page, int size) throws ResultNotFoundException;
-
-	public Pret afficherUnPret(Long id) throws ResultNotFoundException;
-
-	public List<Pret> afficherPrets() throws ResultNotFoundException;
-
-	public List<Pret> afficherPrets(String statut) throws ResultNotFoundException;
-
-	public void modifierStatut(Long id) throws ResultNotFoundException;
-
-	public void modifierStatutsPrets() throws ResultNotFoundException;
-
-	public void modifierPret(Long id, String methode) throws ResultNotFoundException, EntityAlreadyExistException;
-
-}
-=======
 package sid.org.service;
 
 import java.util.List;
@@ -74,4 +39,3 @@ public interface PretService {
 	Optional<Pret> trouverPretenCours(List<Pret> prets);
 
 }
->>>>>>> feature/ListeDattenteReservationLivre
